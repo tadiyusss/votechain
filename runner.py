8a8f220839e106748637cb859b65fbe6746bd494
Thaddeus Toledo <toledothaddeusf@gmail.com>
2024-10-12 00:02:32 +0800

    Updated my runner.py

--- runner.py
+++ runner.py
@@ -4,7 +4,7 @@ from blockchain import Blockchain
 import time
 
 blockchain = Blockchain()
-datas = ['Bob', 'Alice', 'Eve', 'John', 'Doe', 'Jane', 'Smith', 'Johnson']
+datas = ['Bob', 'Alice', 'Eve', 'John']
 
 for i in range(25):
     transactions = []
@@ -19,15 +19,5 @@ for i in range(25):
 
     blockchain.add_block(block.block_values())
     print(f"Time taken: {time.time() - start:.2f} seconds to mine {i + 1} block")
-
-print("Blockchain:")
-for block in blockchain.chain:
-    print(f"Block Hash: {block['block_hash']}")
-    print(f"Merkle Hash: {block['merkle_hash']}")
-    print(f"Nonce: {block['nonce']}")
-    print(f"Previous Block Hash: {block['previous_block_hash']}")
-    for transaction in block['transactions']:
-        print(transaction)
-    print("-" * 50)
 
 print(f"Block validity: {blockchain.verify_block()}")
