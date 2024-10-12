f7fafa0930b6bc2dc19ad41ae8879b632f49bfe1
Thaddeus Toledo <toledothaddeusf@gmail.com>
2024-10-12 10:07:47 +0800

    Added export chain functionality

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,6 @@
 from hashlib import sha256
+import json
+import sqlite3
 
 class Blockchain:
     
@@ -35,9 +37,8 @@ class Blockchain:
         """
         if len(self.chain) == 0:
             return "0"
-        else:
-            return self.chain[-1]['block_hash']
-        
+        return self.chain[-1]['block_hash']
+
     def verify_block(self):
         """
         Will verify the following attributes of each block in the chain:
@@ -65,7 +66,7 @@ class Blockchain:
             if current_block['previous_block_hash'] != previous_block['block_hash']:
                 return False
             
-            if nonce_hash.startswith("0000") == False:
+            if nonce_hash.startswith("0000000") == False:
                 return False
 
             if current_block['block_hash'] != sha256(current_block['previous_block_hash'].encode() + current_block['merkle_hash'].encode()).hexdigest():
@@ -75,3 +76,31 @@ class Blockchain:
                 return False
             
         return True
+    
+    def export_chain(self, filetype = 'json', name = 'blockchain'):
+        """
+        Output the blockchain to a file
+        Inputs:
+            - filetype: [json, sqlite] The file type to output the blockchain to
+        Outputs:
+            - None
+        """
+        if filetype == "json":
+            with open(f'{name}.json', 'w') as file:
+                json.dump(self.chain, file, indent=4)
+
+        elif filetype == "sqlite":
+
+            conn = sqlite3.connect(f'{name}.db')
+            cursor = conn.cursor()
+            cursor.execute("CREATE TABLE IF NOT EXISTS blocks (`block_hash` VARCHAR(64) NOT NULL,`previous_block_hash` VARCHAR(64) NOT NULL, `merkle_hash` VARCHAR(64) NOT NULL, `nonce` INT NOT NULL);")
+            cursor.execute("CREATE TABLE IF NOT EXISTS `transactions` (`block_hash` VARCHAR(64) NOT NULL, `hash` VARCHAR(64) NOT NULL, `timestamp` VARCHAR(30) NOT NULL, `data` VARCHAR(255) NOT NULL );")
+
+            for block in self.chain:
+                sql = "INSERT INTO blocks (block_hash, previous_block_hash, merkle_hash, nonce) VALUES (?, ?, ?, ?)"
+                cursor.execute(sql, (block['block_hash'], block['previous_block_hash'], block['merkle_hash'], block['nonce']))
+                for transaction in block['transactions']:
+                    sql = "INSERT INTO transactions (block_hash, hash, timestamp, data) VALUES (?, ?, ?, ?)"
+                    cursor.execute(sql, (block['block_hash'], transaction['hash'], transaction['timestamp'], transaction['data']))
+            cursor.execute("COMMIT;")
+            cursor.close()
