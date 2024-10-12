3e141253417c379585f5bfe32bf54da2dc494ae1
Thaddeus Toledo <toledothaddeusf@gmail.com>
2024-10-12 10:11:41 +0800

    Changed test file

--- runner.py
+++ runner.py
@@ -2,22 +2,16 @@ from transaction import Transaction
 from block import Block
 from blockchain import Blockchain
 import time
+import json
 
 blockchain = Blockchain()
-datas = ['Bob', 'Alice', 'Eve', 'John']
+transactions = []
 
-for i in range(25):
-    transactions = []
-    for data in datas:
-        transaction = Transaction(data)
-        transactions.append(transaction.transaction_values())
+for _ in range(2):
+    transactions.append(Transaction(input("Enter Data: ")).transaction_values())
 
-    start = time.time()
-    block = Block(transactions, blockchain.get_previous_block_hash())
-    print("Mining block...")
-    block.calculate_nonce()
-
-    blockchain.add_block(block.block_values())
-    print(f"Time taken: {time.time() - start:.2f} seconds to mine {i + 1} block")
-
+block = Block(transactions, blockchain.get_previous_block_hash())
+block.calculate_nonce()
+blockchain.add_block(block.block_values())
+blockchain.output_chain()
 print(f"Block validity: {blockchain.verify_block()}")
