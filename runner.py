14e774d557bc374c2496e87404b40458a1ea04ea
Thaddeus Toledo <toledothaddeusf@gmail.com>
2024-10-15 11:59:35 +0800

    Update for testing the blockchain

--- runner.py
+++ runner.py
@@ -1,17 +1,22 @@
 from transaction import Transaction
 from block import Block
 from blockchain import Blockchain
+from datetime import datetime
 import time
 import json
+from sys import argv
 
 blockchain = Blockchain()
-transactions = []
+data = ['Alice'] * 8
 
-for _ in range(2):
-    transactions.append(Transaction(input("Enter Data: ")).transaction_values())
 
-block = Block(transactions, blockchain.get_previous_block_hash())
-block.calculate_nonce()
-blockchain.add_block(block.block_values())
-blockchain.output_chain()
-print(f"Block validity: {blockchain.verify_block()}")
+if '--export' in argv or '--both' in argv:
+    transactions = [Transaction(data[i]) for i in range(0, len(data))]
+    block = Block(transactions, blockchain.get_previous_block_hash())
+    block.calculate_nonce()
+    blockchain.add_block(block)
+    print(f"Exported chain validity: {blockchain.verify_chain()}")
+    blockchain.export_chain('blockchain.db')
+if '--import' in argv or '--both' in argv:
+    blockchain.import_chain('blockchain.db')
+    print(f"Imported chain validity: {blockchain.verify_chain()}")
