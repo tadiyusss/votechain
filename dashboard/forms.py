28aceef3297908568142acb88a092e0e5265a972
Thaddeus Toledo <toledothaddeusf@gmail.com>
2024-10-20 18:45:56 +0800

    Added voter registration form and private key field in voting form

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,8 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import RadioField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import RadioField, SubmitField, StringField
+from wtforms.validators import DataRequired, Length
 
 class VoteForm(FlaskForm):
     candidates = RadioField('Candidate', choices = [('candidate1', 'Candidate 1'), ('candidate2', 'Candidate 2'), ('candidate3', 'Candidate 3')], validators = [DataRequired()])
+    private_key = StringField('Private Key', validators = [DataRequired(), Length(min = 64, max = 64)])
     submit = SubmitField('Submit')
 
+class RegisterForm(FlaskForm):
+    first_name = StringField('Name', validators = [DataRequired()])
+    last_name = StringField('Last Name', validators = [DataRequired()])
+    username = StringField('Username', validators = [DataRequired(), Length(min = 4, max = 20)])
+    submit = SubmitField('Submit')
